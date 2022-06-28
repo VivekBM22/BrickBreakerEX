aae3ed84a4d60f458ecad2612f9a306dec915b86
AshwinC8 <ashwinsam01@yahoo.com>
2022-06-28 18:55:50 +0530

    Added exit for the menu scene only

--- src/brickBreakerEX/SceneController.java
+++ src/brickBreakerEX/SceneController.java
@@ -3,16 +3,22 @@ package brickBreakerEX;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class SceneController {
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
+	
+	@FXML
+	private AnchorPane Escene;
 	
 	public void SwitchtoMenu( ActionEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
@@ -46,6 +52,11 @@ public class SceneController {
 	
 	public void SwitchtoGameUI( ActionEvent e) {
 		game.GameUI.begin(e);
+	}
+	
+	public void logout(ActionEvent e) {
+		stage = (Stage) Escene.getScene().getWindow();
+		stage.close();
 	}
 	
 }
