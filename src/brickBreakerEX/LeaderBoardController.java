39d516d7018e800acb222a24b3060430b212bd86
Ashwin Sam George <84435023+AshwinC8@users.noreply.github.com>
2022-07-15 00:05:04 +0530

    Movement to leaderboard scene working and new back button :)

--- src/brickBreakerEX/LeaderBoardController.java
+++ src/brickBreakerEX/LeaderBoardController.java
@@ -1,5 +1,24 @@
 package brickBreakerEX;
 
-public class LeaderBoardController {
+import java.io.IOException;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LeaderBoardController {
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	public void switchToMenu( ActionEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+		scene = new Scene(root, 1300, 800);
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		stage.setScene(scene);
+		stage.show();
+	}
 }
