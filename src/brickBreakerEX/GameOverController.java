470f64173114b4a28bbb4f157cc23a39edee1836
AshwinC8 <ashwinsam01@yahoo.com>
2022-07-24 14:09:21 +0530

    added ui for adding and saving  username and highscore  if highscore obtained

--- src/brickBreakerEX/GameOverController.java
+++ src/brickBreakerEX/GameOverController.java
@@ -16,7 +16,8 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class GameOverController {
-	
+	private String username;
+	private int gScore;
 	@FXML
 	Button tryAgain;
 	
@@ -32,11 +33,18 @@ public class GameOverController {
 	@FXML
 	Label enterName;
 	
+	@FXML
+	Button enter;
+	
+	@FXML
+	Button rToMenu;
+	
 	public void lostAll() {
 		message.setText("You Lost");
 		score.setVisible(false);
 		nameTF.setVisible(false);
 		enterName.setVisible(false);
+		enter.setVisible(false);
 	}
 	
 	public void wonLevelSelect() {
@@ -44,10 +52,12 @@ public class GameOverController {
 		score.setVisible(false);
 		nameTF.setVisible(false);
 		enterName.setVisible(false);
+		enter.setVisible(false);
 	}
 	
 	public void wonOtherModes(Integer gameScore) {
 		tryAgain.setVisible(false);
+		rToMenu.setVisible(false);
 		
 		if( GameUI.mode.equals("Tournament") ) {
 			LeaderBoard.setLeaderBoard(LeaderBoard.TM_LB, "TM_LB.txt");
@@ -56,34 +66,41 @@ public class GameOverController {
 				nameTF.setVisible(true);
 				enterName.setVisible(true);
 				score.setText(gameScore.toString());
-				String name = nameTF.getText();
-				LeaderBoard.insertScore( name,  gameScore, LeaderBoard.TM_LB);
+				gScore = gameScore;
+			}else {
+				score.setText(gameScore.toString());
+				nameTF.setVisible(false);
+				enterName.setVisible(false);
+				enterName.setVisible(false);
 			}
 		} else if( GameUI.mode.equals("Time Trial") ) {
 			LeaderBoard.setLeaderBoard(LeaderBoard.TTM_LB, "TTM_LB.txt");
-			if( gameScore > LeaderBoard.TTM_LB.score[9]) {
+			if( gameScore > LeaderBoard.TTM_LB.score[9] ) {
 				message.setText("New Highscore");
 				nameTF.setVisible(true);
 				enterName.setVisible(true);
 				score.setText(gameScore.toString());
-				String name = nameTF.getText();
-				LeaderBoard.insertScore( name,  gameScore, LeaderBoard.TTM_LB);
+				gScore = gameScore;
+			}else {
+				score.setText(gameScore.toString());
+				nameTF.setVisible(false);
+				enterName.setVisible(false);
+				enterName.setVisible(false);
 			}
 		} else if( GameUI.mode.equals("Tournament EX") ) {
 			LeaderBoard.setLeaderBoard(LeaderBoard.THM_LB, "THM_LB.txt");
-			if( gameScore > LeaderBoard.TM_LB.score[9]) {
+			if( gameScore > LeaderBoard.THM_LB.score[9] ) {
 				message.setText("New Highscore");
 				nameTF.setVisible(true);
 				enterName.setVisible(true);
 				score.setText(gameScore.toString());
-				String name = nameTF.getText();
-				LeaderBoard.insertScore( name,  gameScore, LeaderBoard.THM_LB);
+				gScore = gameScore;
+			}else {
+				score.setText(gameScore.toString());
+				nameTF.setVisible(false);
+				enterName.setVisible(false);
 			}
-		} else {
-			score.setText(gameScore.toString());
-			nameTF.setVisible(false);
-			enterName.setVisible(false);
-		}
+		} 
 		
 	}
 	
@@ -98,5 +115,40 @@ public class GameOverController {
 		stage.setScene(scene);
 		stage.show();
 	}
+	
+	public void enteredName(ActionEvent event) {
+		if( GameUI.mode.equals("Tournament") ) {
+			LeaderBoard.setLeaderBoard(LeaderBoard.TM_LB, "TM_LB.txt");
+			if( gScore > LeaderBoard.TM_LB.score[9]) {
+				String name = nameTF.getText();
+				System.out.println(name);
+				LeaderBoard.insertScore( name,  gScore, LeaderBoard.TM_LB);
+				LeaderBoard.setFile(LeaderBoard.TM_LB, "TM_LB.txt");
+			}
+		} else if( GameUI.mode.equals("Time Trial") ) {
+			LeaderBoard.setLeaderBoard(LeaderBoard.TTM_LB, "TTM_LB.txt");
+			if( gScore > LeaderBoard.TTM_LB.score[9]) {
+				String name = nameTF.getText();
+				System.out.println(name);
+				LeaderBoard.insertScore( name,  gScore, LeaderBoard.TTM_LB);
+				LeaderBoard.setFile(LeaderBoard.TTM_LB, "TTM_LB.txt");
+			}
+		} else if( GameUI.mode.equals("Tournament EX") ) {
+			LeaderBoard.setLeaderBoard(LeaderBoard.THM_LB, "THM_LB.txt");
+			if( gScore > LeaderBoard.TM_LB.score[9]) {
+				String name = nameTF.getText();
+				System.out.println(name);
+				LeaderBoard.insertScore( name,  gScore, LeaderBoard.THM_LB);
+				LeaderBoard.setFile(LeaderBoard.THM_LB, "THM_LB.txt");
+			}
+		} 
+		
+		nameTF.setVisible(false);
+		enterName.setVisible(false);
+		enter.setVisible(false);
+		tryAgain.setVisible(true);
+		rToMenu.setVisible(true);
+	}
+	
 	
 }
